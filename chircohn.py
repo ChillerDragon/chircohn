8661479a877e990cd452388f3e483f90c61353bd
ChillerDragon <chillerdragon@gmail.com>
2017-09-13 19:55:41 +0200

    added basic files

---
+++ chircohn.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+#=====================================
+# chircohn compiler by ChillerDragon =
+#=====================================
+
+import os
+
+#TODO: add tmp/ folder where all tmp files get stored
+
+#global vars
+step = 0
+steps = 3
+IsDebug = False
+FoundFuncs = -1
+BaseFuncs = 0
+functions=[["" for x in range(3)] for y in range(2)]
+rcon_cmds=[]
+
+def step_msg(msg):
+	global step
+	global steps
+	print("[" + str(step) + "/" + str(steps) + "] " + msg)
+	step += 1
+
+def clean_file():
+	f = open("tmp_source.txt", "w")
+	with open('testcode.txt',"r") as file:
+		for line in file:
+			if not line.isspace():
+				f.write(line)
+	f.close()
+
+def load_rcon_commands():
+	global rcon_cmds
+	with open("rcon_cmds_vanilla.txt") as f:
+		rcon_cmds = f.readlines()
+	rcon_cmds = [x.strip() for x in rcon_cmds] 
+
+def check_syntax(syntax):
+	for rcon in rcon_cmds:
+		if rcon == syntax:
+			if (IsDebug):
+				print("rcon command found")
+			return 1
+	if function_exist(syntax):
+		if (IsDebug):
+			print("function found")
+		return 2
+	if function_exist(syntax.replace('()','')): #put in two statements for perfomance increase for programs written without ()
+		if (IsDebug):
+			print("function found")
+		return 2
+	print("Syntax error: '" + syntax + "' is no rcon command or function")
+	return -1
+	
+def function_exist(func_name):
+	for name in functions[1]:
+		if name == func_name:
+			return True
+	return False
+
+def pre_create_binary():
+	IsFunc = False
+	lineNUM = 0
+	
+	source_file = open("tmp_binary_no_funcs.txt", "w")
+	clean_file() #create clean tmp file without newlines
+	
+	with open("tmp_source.txt", "rU") as f:
+		for line in f:
+			lineNUM += 1
+			if (line.find("{") != -1):
+				if (IsDebug):
+					print("found { line " + str(lineNUM))
+				IsFunc = True
+			if not line.startswith("void") and not IsFunc:
+				aWords = line.replace('\n','').split(' ')
+				if (check_syntax(aWords[0]) == -1):
+					print("error line " + str(lineNUM))
+					source_file.close()
+					exit()
+				source_file.write(line)
+			if (IsFunc):
+				if (line.find("}") != -1):
+					if (IsDebug):
+						print("found } line " + str(lineNUM))
+					IsFunc = False
+
+	os.remove("tmp_source.txt") #clean temporary files
+	source_file.close()
+
+def create_binary():
+	finish_file = open("compiled_code.txt", "w")
+	pre_create_binary() #removing all function definitions and cleaning newlines
+	
+	with open("tmp_binary_no_funcs.txt", "rU") as f:
+		for line in f:
+			aWords = line.replace('\n','').split(' ')
+			if (check_syntax(aWords[0]) == 2):
+				aWords[0] = aWords[0].replace('()','')
+				func_index = -1
+				for i in range(FoundFuncs + 1):
+					if functions[1][i] == aWords[0]:
+						func_index = i
+				
+				if func_index == -1:
+					print("Error loading function " + aWords[0] + "()")
+					finish_file.close()
+					exit()	
+				#replace function calls with the function code
+				finish_file.write(functions[0][func_index])
+			else: #no function --> just copy the plain code
+				finish_file.write(line)
+
+	os.remove("tmp_binary_no_funcs.txt") #clean temporary files
+	finish_file.close()
+	
+def init_base_functions():
+	global BaseFuncs
+	global FoundFuncs
+
+	functions[1][0] = "HelloWorld"
+	functions[0][0] = "echo helloworld"
+	BaseFuncs += 1
+	
+	functions[1][1] = "test"
+	functions[0][1] = "echo test"
+	BaseFuncs += 1
+	
+	functions[1][2] = "test2"
+	functions[0][2] = "echo test2\necho test2\necho test2"
+	BaseFuncs += 1
+	
+	FoundFuncs += BaseFuncs
+
+def load_user_functions():
+	global FoundFuncs
+	IsFunc = False
+	with open("testcode.txt", "rU") as f:
+		for line in f:
+			if line.startswith("void"):
+				FoundFuncs += 1
+				aWords = line.replace('()', '').replace('\n','').split(' ') #fish the function name		
+				if function_exist(aWords[1]):
+					print("ERROR function " + aWords[1] + " already exsists")
+					exit()
+				functions[1].append(aWords[1]) #store the function name in func array
+				functions[0].append("") #add empty array part for code
+			if (IsFunc):
+				if (line.find("}") != -1):
+					IsFunc = False
+				else:
+					aWords = line.replace('()', '').replace('\n','').split(' ')
+					if (check_syntax(aWords[0]) == -1):
+						print("Syntax error in function " + functions[1][FoundFuncs] + "()")
+						exit()
+					functions[0][FoundFuncs] += line
+					
+			if (line.find("{") != -1):
+				IsFunc = True
+	
+	
+	
+	
+
+	
+def compile_main():
+	global FoundFuncs
+	global step
+	global steps
+	print("===================================")
+	print("ChillerDragon's chircohn compiler")
+	print("===================================")
+	step_msg("init base functions")
+	init_base_functions()
+	step_msg("load rcon commands")
+	load_rcon_commands()
+	
+	load_user_functions()
+	if (FoundFuncs and IsDebug):
+		for i in range(FoundFuncs + 1):
+			print("--------  Index[" + str(i) + "]  -----------")
+			print("Name: " + functions[1][i])
+			print("Code: \n" + functions[0][i])
+			print("functions: " + str(FoundFuncs) + " + 1")
+	step_msg("loading functions succeded (" + str(FoundFuncs) + ")")
+	create_binary()
+	
+	
+compile_main()
